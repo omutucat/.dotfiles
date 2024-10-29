3a1d5d268cedfbfc1367c35d96f529d1151f66b9
omutucat <y.kuma1102@gmail.com>
2024-10-29 21:34:42 +0900

    update wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,7 +5,7 @@ local config = wezterm.config_builder()
 config.font = wezterm.font("HackGen35 Console NF", { bold = true, italic = false })
 config.use_ime = true
 config.font_size = 10.0
-config.color_scheme = "OneHalfDark"
+config.color_scheme = "arcoiris"
 config.hide_tab_bar_if_only_one_tab = true
 config.adjust_window_size_when_changing_font_size = false
 config.default_prog = { 'nu' }
