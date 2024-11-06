71b606d9789f530b639ca35eb74386a9ebfb429a
omutucat <y.kuma1102@gmail.com>
2024-11-06 22:43:25 +0900

    update wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,7 +8,7 @@ config.font_size = 10.0
 config.color_scheme = "arcoiris"
 config.hide_tab_bar_if_only_one_tab = true
 config.adjust_window_size_when_changing_font_size = false
-config.default_prog = { 'nu' }
+config.default_prog = { 'pwsh' }
 config.window_background_opacity = 0.85
 
 
